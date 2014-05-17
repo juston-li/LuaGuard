89cd849407d5412ec2c300bd0eb6956e9c05463a
t3dodson <t3.dodson@gmail.com>
2014-05-17 16:26:30 -0700

    recursive tree finder update

---
+++ LuaGuardCLI/src/Obfuscator.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.util.ArrayList;
+
+interface Obfuscator {
+	/*
+	Obfuscator Interface:
+	This interface specifies the requirements obfuscate an AST file
+	*/
+	
+	void obfuscate(File ast, ArrayList<String> blacklist);
+	/*
+	
+	ast: The ast file to be obfuscated using either walker.js or GSON
+	
+	blacklist: A list of keywords and identifiers not to be obfuscated
+		The blacklist should be checked before modifying any identifier.
+	
+	The returned File should be the overwritten file that is the result
+	of the obfuscation method.
+	*/
+}
