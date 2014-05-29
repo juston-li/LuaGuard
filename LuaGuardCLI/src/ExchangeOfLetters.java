218133c0941bd29796002da886053ac1dc0d2f44
t3dodson <t3.dodson@gmail.com>
2014-05-28 23:04:05 -0700

    Encorporated the other parser and removed xor Codes are now 1 2 3 for obfuscation

--- LuaGuardCLI/src/ExchangeOfLetters.java
+++ LuaGuardCLI/src/ExchangeOfLetters.java
@@ -8,9 +8,12 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
 
-public class StringEncryption {
+
+public class ExchangeOfLetters {
+	
 	
 	private walker TexasRanger = new walker();
+	private int rand = Integer.MAX_VALUE;
 	
 	public void obfuscate(File ast, ArrayList<String> bl){		
 		TexasRanger.addToBlacklist(bl);
@@ -60,7 +63,7 @@ public class StringEncryption {
 							} else if (i == 0 && !TexasRanger.check_blacklist(str)){
 								// check if in blacklist and previously obfuscated
 								if (!TexasRanger.check_name(str)){
-									String ob_name = xor(str,(int)Math.random(),(byte)'x');
+									String ob_name = EOFProduce(str);
 									TexasRanger.obfuscated_names.put(str, ob_name);
 									writer.value(ob_name);
 								} else {
@@ -93,107 +96,34 @@ public class StringEncryption {
 		}
 	
 	
-	public String xor (String  str,int num,byte xorchar)
-	 {
-		 //for(int i=0;i<str.length;i++)
-		 //{
-			 byte [] xor=str.getBytes();                             
-			 for(int j=0;j<xor.length;j++)
-			 {
-				 xor[j]=(byte)(xorchar^xor[j]);                             
-			 }
-			 //int choice=num%2;
-			 switch(num%2)                                                   
-			 {
-			 case 0:for(int j=0;j<xor.length/2;j++)
-			 		{
-				 		byte mid=xor[j];
-				 		xor[j]=xor[xor.length-j-1];
-				 		xor[xor.length-j-1]=mid;
-			 		}
-			 		break;		
-			 case 1:for(int j=0;j<xor.length;j+=2)
-			 		{
-				 		byte mid=xor[j];
-				 		if(j+1<xor.length)
-				 		{
-				 			xor[j]=xor[j+1];
-				 			xor[j+1]=mid;
-				 		}
-			 		}
-			 		break;
-			 	default:break;
-		 }
-			//str[i]=new String(xor);
-			 byte [] nxor=change(xor);                                   
-			 str=new String(nxor);
-			 return str;
-		 //}
-	 }
-
-	private byte[]  change(byte [] byt)
-	{
-		int i=0;
-		int num=0;
-		while(i<byt.length)
-		{
-			if(byt[i]<0)
-				i+=2;
-			else
-				if(byt[i]>=32&&byt[i]<127)
-				i++;
-				else
-				{
-					num++;
-					i++;
-					
-				}
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * This function changes the letters order of the variable name. For an example, 
+	 * the name "abcd" after obfuscation would become "_dbca".
+	 * @param name: the name you want to obfuscate
+	 * @return: the new name
+	 */
+	public String EOFProduce(String name){
+		//System.out.println("old name is " + name);
+		
+		char[] result = name.toCharArray();
+		if(name.length() <= 0)
+			return "";
+		for(int i = 0; i < name.length() / 2; ){
+			char temp = result[i];
+			result[i] = result[name.length() - 1 - i];
+			result[name.length() - 1 - i] = temp;
+			i = i + 2;
 		}
-	num=3*num+byt.length;
-		byte [] newbyt=new byte[num];
-		int pass=0;
-		for(i=0;i<byt.length;i++)
-		{
-			if(byt[i]<0)
-			{
-				
-				newbyt[pass++]=byt[i++];
-				if(i<byt.length)
-					newbyt[pass++]=byt[i];
-			}
-			else
-				if(byt[i]>=32&&byt[i]<127 && byt[i]!='\"' && byt[i]!='\\')
-				{
-					newbyt[pass++]=byt[i];
-				}
-				else
-				{
-					if(pass<num)
-					{
-						String xx="\\x";
-						byte [] s=xx.getBytes();
-						if(s.length>=2)
-						{
-						newbyt[pass++]=s[0];
-						newbyt[pass++]=s[1];
-						}
-						String hex=Integer.toHexString(byt[i]);
-						byte [] bu=hex.getBytes();
-						if(bu.length>=2)
-						{
-							newbyt[pass++]=bu[0];
-							newbyt[pass++]=bu[1];
-						}
-						else
-							{
-							newbyt[pass++]='0';
-							newbyt[pass++]=bu[0];
-							}
-					}
-				}
-		}
-		return newbyt;
+		
+		//System.out.println("new name is _" + String.valueOf(result));
+		return ("_" + String.valueOf(result));
 	}
-
 }
-
