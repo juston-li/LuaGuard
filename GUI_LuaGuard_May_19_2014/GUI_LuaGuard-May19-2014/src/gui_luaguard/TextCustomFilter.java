2b3d1dd43832c78c577ba5ee0a42b67d45ff41f4
will <will@cs.uoregon.edu>
2014-05-29 16:31:30 -0700

    Add gui subdirectory

---
+++ GUI_LuaGuard_May_19_2014/GUI_LuaGuard-May19-2014/src/gui_luaguard/TextCustomFilter.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gui_luaguard;
+
+import java.io.File;
+
+/**
+ *
+ * @author willmbp15
+ */
+class TextCustomFilter extends javax.swing.filechooser.FileFilter {
+  
+  @Override
+  public boolean accept(File file) {
+    // Allow only directories, or files with ".lua" extension
+    return file.isDirectory() || file.getAbsolutePath().endsWith(".txt");
+  }
+  @Override
+  public String getDescription() {
+    // This description will be displayed in the dialog,
+    // hard-coded = ugly, should be done via I18N
+    return "Text documents (*.txt)";
+  }
+  
+}
